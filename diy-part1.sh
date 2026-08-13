a8fd64e285495d3360592a6c48987a93722f4ad9
PoorMilk <86163938+PoorMilk@users.noreply.github.com>
2026-08-13 23:06:01 +0800

    Update and rename diy.sh to diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+
+# 如果以后需要添加额外的第三方插件源，可以在下面取消注释或添加 echo
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
